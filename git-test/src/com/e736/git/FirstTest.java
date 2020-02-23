9cbcb2b835b28821c6247efd7e9b65fe8ba0d126
wwlearn <ww.learn@163.com>
2020-02-23 10:32:51 +0800

    git-test加入到git

---
+++ git-test/src/com/e736/git/FirstTest.java
@@ -0,0 +1,11 @@
+package com.e736.git;
+
+public class FirstTest {
+    public static void main(String[] args) {
+        System.out.println("hello,word");
+        System.out.println("hello,word");
+        System.out.println("hello,word");
+        System.out.println("hello,word");
+        System.out.println("hello,word");
+    }
+}
